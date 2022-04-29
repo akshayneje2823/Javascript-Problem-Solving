db59808b34c9c1bb89919d075df6d8fed7d32964
Akshay Neje <akshayneje2823@gmail.com>
2022-04-29 23:44:56 +0530

    Logical Questions

---
+++ logical/Swap.js
@@ -0,0 +1,28 @@
+// Wap to progarm swap two numbers without using third variable
+
+
+var a = 10
+var b = 100
+
+/*
+
+ First Way
+ a = a+b   // a = 10+100 = 110
+ b = a-b   // b = 110-100 = 10
+ a = a-b   // a = 110-10 = 20 
+ console.log(a)
+ console.log(b) 
+
+ */
+secondMethod
+ /*
+ var c = 22
+var d = 33
+c = (c*d)/(d=c)]
+
+  */
+var c = 22
+var d = 33
+c = (c*d)/(d=c)
+console.log(c)
+ console.log(d) 
