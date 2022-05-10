3518d04ab7a05357f3b672b902821fc146cf74fb
Akshay Neje <akshayneje2823@gmail.com>
2022-05-10 23:47:02 +0530

    Done

---
+++ Array Examle/Array length property.js
@@ -0,0 +1,5 @@
+const clothes = ["java","Jacket"]
+clothes.length = 0;
+
+// 
+clothes[0]       /* undefined */
---
+++ Array Examle/Eagle eyes test.js
@@ -0,0 +1,13 @@
+const length  = 4;
+const number = [];
+
+var i = 0;
+for(i=0;i<length;i++);{
+    number.push(i+1)
+}
+
+number;    /* ==> ???   */
+
+
+console.log(number)
+//  Ans ==> 5
