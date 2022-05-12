eb84e07d51d0b0c610e13b17e8ea0935c60d8c6c
Akshay Neje <akshayneje2823@gmail.com>
2022-05-13 00:15:25 +0530

    Done

--- Reverse/string.js
+++ Reverse/string.js
@@ -1,9 +1,24 @@
-function revString(str) {
-    var newString = "";
-    for (var i = str.length - 1; 1 >= 0; i--) {
-        newString = newString + str[i]
-    }
-    return newString;
+// function revString(str) {
+//     var newString = "";
+//     for (var i = str.length - 1; 1 >= 0; i--) {
+//         newString = newString + str[i]
+//     }
+//     return newString;
+// }
+// let result = revString("Akshay")
+// console.log(result)
+
+let reverseString = (str) =>{
+    let arr = str.split("");
+    arr.reverse();
+    str = arr.join("");
+    console.log(str)
 }
-let result = revString("Akshay")
-console.log(result)
+
+reverseString("Apple")
+
+/* Solution
+step 1 => Convert in to Array
+step 2 => lopp through each other
+step 3 => return the reverse
+*/
