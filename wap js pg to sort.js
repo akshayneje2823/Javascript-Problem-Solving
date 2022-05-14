f41a9e2588013fcf34ac45d3e2481158796dde92
Akshay Neje <akshayneje2823@gmail.com>
2022-05-14 23:05:29 +0530

    done

--- wap js pg to sort.js
+++ wap js pg to sort.js
@@ -1,4 +1,5 @@
-let arr = [1,0,2,0,30,5]
+let arr = [1,0,2,0,30,5,0,3,0]
+// output ==> [0,0,0,0,0,1,2,3,30,5]
 // let srtArr = arr.sort((a,b)=>{b-a})
 // console.log(srtArr)
 console.log(arr.sort())
