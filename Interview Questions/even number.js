d42f0cd9ba216e82350e1270729d12b2396827a0
Akshay Neje <akshayneje2823@gmail.com>
2022-09-10 22:25:27 +0530

    Done

--- Interview Questions/even number.js
+++ Interview Questions/even number.js
@@ -1,11 +1,3 @@
-function is_evenn(n){
-    if (n < 0) {
-        return false
-    } 
-    if (n < 0) {
-        return false
-    } 
-    if (n < 0) {
-        return false
-    } 
-}
+const is_Even = num => num % 2 === 0;
+console.log(is_Even(18))
+console.log(is_Even(79))
