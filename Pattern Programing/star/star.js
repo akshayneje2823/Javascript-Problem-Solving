bb9ab00eb6258aaf13592a3fca7f4c299bea96de
Akshay Neje <akshayneje2823@gmail.com>
2022-09-15 19:02:02 +0530

    Done

--- Pattern Programing/star/star.js
+++ Pattern Programing/star/star.js
@@ -1,8 +1,16 @@
-for (let i = 0; i <= 5; i++) {
-    console.log("*");
-    for (let j = 0; j <= i; j++) {
-        console.log("*");
-        
-    }
-    console.log('/n')
+// for (let i = 0; i <= 5; i++) {
+//     console.log("*");
+//     for (let j = 0; j <= i; j++) {
+//         console.log("*");    
+//     }
+//     console.log('/n')
+// }
+let str = "";
+
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= i; j++) {
+    str =str + "*";
+  }
+  str = str + "\n"
 }
+console.log(str)
