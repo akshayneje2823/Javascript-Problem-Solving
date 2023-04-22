cdc61b3c09eb62a893b070147792d50bed2529d3
Akshay Neje <akshayneje2823@gmail.com>
2023-04-23 01:29:58 +0530

    Done

--- Pattern Programing/leftAngle.js
+++ Pattern Programing/leftAngle.js
@@ -19,3 +19,24 @@
 // console.log(str);
 
 
+
+// Downward Triangle Star Pattern
+let n = 5;
+
+let str = "";
+
+
+for (let row = 1; row <= n; row++) {
+
+    for (let column = 0; column <= n-row; column++) {
+        str += "*"
+    }
+
+    str += '\n'
+}
+
+
+console.log(str);
+
+
+
